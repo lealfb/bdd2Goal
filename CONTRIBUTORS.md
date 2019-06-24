4a09a5b71dd5d7de0bd7bbfa4cd690b0915f81e7
lealfb <leal.fb@gmail.com>
2019-06-24 13:49:32 -0300

    Limpeza para atualização do fonte original do piStar para versão 2.0

--- CONTRIBUTORS.md
+++
@@ -1,5 +0,0 @@
-﻿# List of Contributors
-*To see which contributions each individual made to this project, check our commit history*
-
-
-* João Pimentel
--- tool/js/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    istar.setupModel();
-    istar.setupDiagram();
-    istar.setupMetamodel(istarcoreMetamodel);
-    istar.examples.loadPistarWelcome();
-    ui.setupUi();
-});
