06e9a10510cfd7f923d9a5b9183c14d28f2cc1db
lealfb <leal.fb@gmail.com>
2019-07-01 15:36:11 -0300

    Alteração para ler o result dos arquivos e associar as cores ao modelo, para salvar e carregar de arquivo com as cores predefinidas para os seus respectivos resultados.

--- tool/app/ui/pistar_plugin.js
+++ tool/app/ui/pistar_plugin.js
@@ -1,25 +1,32 @@
 var fList = new Array();
 var titulos = new Array();
 
+var colours = [
+    {colour: 'SUCCESS', hex: '#6CFA4B'},
+    {colour: 'FAILURE', hex: '#FA7267'},
+    {colour: 'PENDING', hex: '#B4B8FA'},
+    {colour: 'SKIPPED', hex: '#FFFFFF'},
+];
+
 $.ajax({
     url: "json/",
     success: function (data) {
-	$(data).find("a").each(function (a, b) {
-	    if (/.+\.json/.test(b.href)) {
-		var href = b.href;
-		var base = href.replace(/\/[a-f|A-F|0-9]+.json$/, '');
-		var href = href.replace(base, '');
-		fList.push('json' + href);
-	    }
-	});
+        $(data).find("a").each(function (a, b) {
+            if (/.+\.json/.test(b.href)) {
+                var href = b.href;
+                var base = href.replace(/\/[a-f|A-F|0-9]+.json$/, '');
+                var href = href.replace(base, '');
+                fList.push('json' + href);
+            }
+        });
     }
 });
 
 function loadNames() {
     fList.forEach(function (jsonFile) {
-	$.getJSON(jsonFile, function (data) {
-	    titulos.push(data.name);
-	});
+        $.getJSON(jsonFile, function (data) {
+            titulos.push({name: data.name.replace("'", ' '), result: data.result});
+        });
     });
 }
 
@@ -32,19 +39,19 @@ function renderComplexityTemplate(propertyName, propertyValue) {
 
     complexities.forEach(function (listValue) {
 
-	if (listValue == propertyValue) {
-	    selectbox += "<option selected value='" + listValue + "'>" + listValue + "</option>";
-	} else {
-	    selectbox += "<option value='" + listValue + "'>" + listValue + "</option>";
-	}
+        if (listValue == propertyValue) {
+            selectbox += "<option selected value='" + listValue + "'>" + listValue + "</option>";
+        } else {
+            selectbox += "<option value='" + listValue + "'>" + listValue + "</option>";
+        }
     });
 
     selectbox += '</select>';
 
     var template = '<tr>' +
-	    '<th scope="row" class="property-name">' + propertyName + '</th>' +
-	    '<td class="property-value custom-property-value">' +
-	    selectbox + '</td></tr>';
+            '<th scope="row" class="property-name">' + propertyName + '</th>' +
+            '<td class="property-value custom-property-value">' +
+            selectbox + '</td></tr>';
 
     return template;
 }
@@ -58,56 +65,72 @@ function renderCustomPropertyTemplate(propertyName, propertyValue) {
     selectbox += "<option value=''>Selecione</option>";
 
     titulos.forEach(function (listValue) {
-
-	listValue = listValue.replace("'", ' ');
-
-	if (listValue == propertyValue) {
-	    selectbox += "<option selected value='" + listValue + "'>" + listValue + "</option>";
-	} else {
-	    selectbox += "<option value='" + listValue + "'>" + listValue + "</option>";
-	}
+        var label = listValue.result + ':' + listValue.name;
+        var value = listValue.name;
+        var selected = (listValue.name == propertyValue) ? 'selected' : '';
+        selectbox += "<option " + selected + " value='" + value + "'>" + label + "</option>";
     });
 
     selectbox += '</select>';
 
     var template = '<tr>' +
-	    '<th scope="row" class="property-name">' + propertyName + '</th>' +
-	    '<td class="property-value custom-property-value">' +
-	    selectbox + '</td></tr>';
+            '<th scope="row" class="property-name">' + propertyName + '</th>' +
+            '<td class="property-value custom-property-value">' +
+            selectbox + '</td></tr>';
 
     return template;
 }
 
+function getColour(result) {
+    for (var i = 0; i < colours.length; i++) {
+        if (colours[i].colour == result) {
+            return colours[i].hex;
+        }
+    }
+}
+
+function getResultColour(val) {
+    for (var i = 0; i < titulos.length; i++) {
+        if (titulos[i].name == val) {
+            return getColour(titulos[i].result);
+        }
+    }
+}
+
 function customChangeValue(source) {
+    
     var selectedElement = ui.getSelectedCells()[0];
+    var colour = getResultColour($(source).val());
+    
     ui.changeCustomPropertyValue(selectedElement, $(source).prop('name'), $(source).val());
+    ui.changeColorElement(colour);
 }
 
 ui.components.PropertiesTableView.prototype.renderCustomProperty = function (propertyName) {
-    
+
     if (this.model.attributes.type == 'Task') {
 
-	var customProperties = this.model.attributes.customProperties;
+        var customProperties = this.model.attributes.customProperties;
 
-	var keys = Object.keys(customProperties);
-	
-	var customTemplate = null;
+        var keys = Object.keys(customProperties);
 
-	switch (keys.indexOf(propertyName)) {
-	    case 1:
-		customTemplate = renderCustomPropertyTemplate(propertyName, this.model.prop('customProperties/' + propertyName));
-		break;
-	    case 2:
-		customTemplate = renderComplexityTemplate(propertyName, this.model.prop('customProperties/' + propertyName));
-		break;
-	    default:
-		customTemplate = this.template({
-		    propertyName: propertyName,
-		    propertyValue: this.model.prop('customProperties/' + propertyName),
+        var customTemplate = null;
+
+        switch (keys.indexOf(propertyName)) {
+            case 1:
+                customTemplate = renderCustomPropertyTemplate(propertyName, this.model.prop('customProperties/' + propertyName));
+                break;
+            case 2:
+                customTemplate = renderComplexityTemplate(propertyName, this.model.prop('customProperties/' + propertyName));
+                break;
+            default:
+                customTemplate = this.template({
+                    propertyName: propertyName,
+                    propertyValue: this.model.prop('customProperties/' + propertyName),
                     dataType: 'textarea'
-		});
-	}
-        
+                });
+        }
+
     } else {
         this.$table.find('tbody').append(this.template({
             propertyName: propertyName,
@@ -120,5 +143,5 @@ ui.components.PropertiesTableView.prototype.renderCustomProperty = function (pro
 };
 
 $(document).ready(function () {
-    setTimeout('loadNames()', 500);
+    setTimeout('loadNames()', 1000);
 });
