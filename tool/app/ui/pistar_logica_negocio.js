23aa5a7583e8dda67e98318f781cf054c819f113
lealfb <leal.fb@gmail.com>
2019-08-21 17:46:31 -0300

    Correção campos, customProperties

--- tool/app/ui/pistar_logica_negocio.js
+++ tool/app/ui/pistar_logica_negocio.js
@@ -127,14 +127,6 @@ function getChild(id) {
     return child;
 }
 
-function normalize(string) {
-    return string.toLocaleString().replace(/\s+/g, '').trim().normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
-}
-
-function compareNames(name1, name2) {
-    return normalize(name1) == normalize(name2);
-}
-
 // DEBUG ABAIXO
 
 var missing = [];
@@ -198,49 +190,3 @@ function diffTasks() {
     
     return missing;
 }
-
-function similarity(s1, s2) {
-    
-    s1 = normalize(s1);
-    s2 = normalize(s2);
-    
-    var longer = s1;
-    var shorter = s2;
-    if (s1.length < s2.length) {
-        longer = s2;
-        shorter = s1;
-    }
-    var longerLength = longer.length;
-    if (longerLength == 0) {
-        return 1.0;
-    }
-    return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength);
-}
-
-function editDistance(s1, s2) {
-    s1 = s1.toLowerCase();
-    s2 = s2.toLowerCase();
-
-    var costs = new Array();
-    for (var i = 0; i <= s1.length; i++) {
-        var lastValue = i;
-        for (var j = 0; j <= s2.length; j++) {
-            if (i == 0)
-                costs[j] = j;
-            else {
-                if (j > 0) {
-                    var newValue = costs[j - 1];
-                    if (s1.charAt(i - 1) != s2.charAt(j - 1))
-                        newValue = Math.min(Math.min(newValue, lastValue),
-                                costs[j]) + 1;
-                    costs[j - 1] = lastValue;
-                    lastValue = newValue;
-                }
-            }
-        }
-        if (i > 0)
-            costs[s2.length] = lastValue;
-    }
-    return costs[s2.length];
-}
-
